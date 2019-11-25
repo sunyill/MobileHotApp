9c23b3ca1f2fd0d54334e8241f03625f995d4c81
sunyill <sunyill0707@163.com>
2019-11-25 11:10:33 +0800

    4. 项目|json-bigint大数字处理

--- src/Utils/request.js
+++ src/Utils/request.js
@@ -3,12 +3,26 @@
  */
 
 import axios from 'axios'
+import JsonBigInt from 'json-bigint'
 
 // 创建一个axios实例,配置不同的baseUrl
 const Instance = axios.create({
   timeout: 5000,
-  baseURL: 'http://ttapi.research.itcast.cn'
+  baseURL: 'http://ttapi.research.itcast.cn',
+  /**
+ * 在服务器返回数据时,进行大数字处理
+ * transfromResponse 是在响应拦截之前执行
+ */
+  transformResponse: [function (data) {
+    // 数据data可能不是标准的json格式,会报错,这时将data原样return
+    try {
+      return JsonBigInt.parse(data)
+    } catch (err) {
+      return data
+    }
+  }]
 })
+
 /**
  * 请求
  */
